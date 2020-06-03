7b70492e873b5d96f64e3bc4a514f92342f08f84
Ryan Afshar <ryanafsharryan@gmail.com>
2020-06-03 11:45:45 -0700

    Add Game Manager, responsible for loading scenes and levels

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace Com.MyCompany.MyGame
+{
+    public class GameManager : MonoBehaviourPunCallbacks
+    {
+
+        #region Photon Callbacks
+
+        // Called when the local player left the room. We need the load the launcher scene.
+        public override void OnLeftRoom()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void LeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+
+        #endregion
+    }
+}
